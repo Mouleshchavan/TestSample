0a11bd6fe713b8a31e96a93a369f517577098b52
mouleshchavan <mouleshchavan@gmail.com>
2023-10-18 21:31:39 +0530

    updatedCode

---
+++ task/taskFront/my-fullstack-app/src/components/ProductsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductsPage.css'; // Add CSS for this component
+
+function ProductsPage() {
+  return (
+    <div className="products-page">
+      {/* Content of your Products Page */}
+    </div>
+  );
+}
+
+export default ProductsPage;
