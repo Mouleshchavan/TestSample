8eb52049fffa2a530142a0c7867baea322956fee
Moulesh <mouleshchavan@gmail.com>
2023-10-18 19:13:55 +0530

    Backend Task Completed

---
+++ taskFront/my-fullstack-app/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      {/* Add content for your Contact page */}
+    </div>
+  );
+}
+
+export default Contact;
---
+++ taskFront/my-fullstack-app/src/components/RegistrationPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RegistrationPopup.css'; // Add CSS for this component
+
+function RegistrationPopup() {
+  return (
+    <div className="registration-popup">
+      {/* Content of your Registration Popup */}
+    </div>
+  );
+}
+
+export default RegistrationPopup;
